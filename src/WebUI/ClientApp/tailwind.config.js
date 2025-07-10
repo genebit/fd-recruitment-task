ae705c598ab6da899ccf8c105f388dd0cc68db07
Johcel Gene T. Bitara • BS IT <jbitara@gbox.adnu.edu.ph>
2025-07-10 23:11:11 +0800

    chore(pckg): added tailwindcss to project.

---
+++ src/WebUI/ClientApp/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  important: true,
+  prefix: "tw-",
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
